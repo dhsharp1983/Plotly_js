f008251768c41a5a804a0165e9975a441abc1b24
dhsharp1983 <sharpdavid83@gmail.com>
2021-05-22 18:35:37 +1000

    update

--- static/js/app.js
+++ static/js/app.js
@@ -16,6 +16,12 @@ function unpack(rows, key) {
     });
 };
 
+function convertAsString(val) {
+    var str1 = "OTU ";
+    var str2 = val.toString();
+    var result = str1.concat(str2);
+    return result;
+};
 
 function ReadInJSONData(jsonfile) {
     d3.json(jsonfile).then((data) => {
@@ -39,14 +45,42 @@ function mainFunction () {
 
 function SortData(inputdata) {
     console.log("ReturnData Function");
-    console.log("ReturnData type is: " + typeof(importedData) + ", data below");
+    console.log("ReturnData type is: " + typeof(inputdata) + ", data below");
     // console.log(inputdata);
-    Object.keys(inputdata.samples[0]).forEach(key => console.log(key));
-    var sample_values = Object.values(inputdata.samples[0].sample_values);
-    var otu_ids = Object.values(inputdata.samples[0].otu_ids);
+    // Object.keys(inputdata.samples[0]).forEach(key => console.log(key));
+    // Object.values(inputdata.samples[0]).forEach(value => console.log(value))
+    // start prev good code 
+    // var sample_values = Object.values(inputdata.samples[0].sample_values);
+    // var otu_ids = Object.values(inputdata.samples[0].otu_ids);
+    // var samples_to_otu = [];
+    // otu_ids.forEach((key, i) => samples_to_otu[key] = sample_values[i]);
+    // Object.keys(samples_to_otu).forEach(key => console.log(key));
+    // Object.values(samples_to_otu).forEach(value => console.log(value));
+    // end prev good code 
+    // SamplesArray = Object.entries(inputdata.samples[0]["otu_ids"])
+    var otu_ids = Object.values(inputdata.samples[0]["otu_ids"]).slice(0,11)
+    var sample_values = Object.values(inputdata.samples[0]["sample_values"]).slice(0,11)
+    otu_ids.forEach(element => {
+        console.log(element)
+    });
+    sample_values.forEach(element => {
+        console.log(element)
+    });
+    otu_ids_str = otu_ids.map(convertAsString);
+    otu_ids_str.forEach(element => {
+        console.log(element)
+    });
+    // var sample_values = Object.values(SamplesArray[2]).slice(0,11)
+    // otu_ids.forEach(([key,value]) => {
+    //     console.log(key);
+    //     console.log(value);
+    // });
+
+    console.log(typeof(otu_ids))
     // var sample_values = Object.values(inputdata.samples[0].sample_values).forEach(value => console.log(value));
     // var otu_ids = Object.values(inputdata.samples[0].otu_ids).forEach(value => console.log(value));
-    BuildGraph(otu_ids, sample_values);
+    // numArray3.sort((firstNum, secondNum) => firstNum - secondNum);
+    BuildGraph(otu_ids_str, sample_values);
 };
 
 
@@ -57,9 +91,9 @@ function BuildGraph(x_data,y_data) {
         type: "bar"
     };
     console.log("BuildGraph")
-    x_data.forEach(element => {
-        console.log(element)
-    });
+    // x_data.forEach(element => {
+    //     console.log(element)
+    // });
     var data = [trace1];
 
     var chartLayout = {
