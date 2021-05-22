d46cf85886b9d240758ef5f03b2a14e65cf8b6c8
dhsharp1983 <sharpdavid83@gmail.com>
2021-05-22 23:34:04 +1000

    update

--- static/js/app.js
+++ static/js/app.js
@@ -9,6 +9,9 @@ jsonfile = "data/samples.json";
 //     console.log("Data Read");
 //     return JSONdata;
 // };
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
 
 function unpack(rows, key) {
     return rows.map(function(row) { 
@@ -30,7 +33,12 @@ function ReadInJSONData(jsonfile) {
         var importedData = data;
         console.log(importedData);
         console.log(" importedData is a " + typeof(importedData));
+
+        var names_list = Object.values(importedData.names)
+        // names_list.forEach(name => console.log(name));
+        PopulateDropDown (names_list);
         SortData(importedData);
+
         // var dates = importedData.map()
     // .catch(function(error) {
     //     console.log("JOSN data Error!")
@@ -38,57 +46,67 @@ function ReadInJSONData(jsonfile) {
 };
 
 
+function PopulateDropDown (names) {
+    console.log("populate drop down")
+    names.forEach(name => console.log(name))
+    var dropDown = d3.select("#selDataset");
+
+    var options = dropDown.selectAll("option")
+                    .data(names)
+                    .enter()
+                    .append("option");
+
+    options.text(function (d) {return d;});
+};
+
 // var JSONdatatest = ReadInJSONData(jsonfile);
 function mainFunction () {
     ReadInJSONData(jsonfile)
 };
 
 function SortData(inputdata) {
+    var names_list = Object.values(inputdata.names)
+    var candidate_id = d3.select("#selDataset").node().value;
+    console.log("name", candidate_id)
+    
+    var candidate_index = names_list.indexOf(candidate_id);
+    console.log("Candidate Index", candidate_index);
+
     console.log("ReturnData Function");
     console.log("ReturnData type is: " + typeof(inputdata) + ", data below");
-    // console.log(inputdata);
-    // Object.keys(inputdata.samples[0]).forEach(key => console.log(key));
-    // Object.values(inputdata.samples[0]).forEach(value => console.log(value))
-    // start prev good code 
-    // var sample_values = Object.values(inputdata.samples[0].sample_values);
-    // var otu_ids = Object.values(inputdata.samples[0].otu_ids);
-    // var samples_to_otu = [];
-    // otu_ids.forEach((key, i) => samples_to_otu[key] = sample_values[i]);
-    // Object.keys(samples_to_otu).forEach(key => console.log(key));
-    // Object.values(samples_to_otu).forEach(value => console.log(value));
-    // end prev good code 
-    // SamplesArray = Object.entries(inputdata.samples[0]["otu_ids"])
-    var otu_ids = Object.values(inputdata.samples[0]["otu_ids"]).slice(0,11)
-    var sample_values = Object.values(inputdata.samples[0]["sample_values"]).slice(0,11)
-    otu_ids.forEach(element => {
-        console.log(element)
-    });
-    sample_values.forEach(element => {
-        console.log(element)
-    });
+
+    // Object.keys(inputdata.samples[0]).forEach(key => console.log(key))
+    // bar data 
+    var otu_ids = Object.values(inputdata.samples[candidate_index]["otu_ids"]).slice(0,11)
+    var sample_values = Object.values(inputdata.samples[candidate_index]["sample_values"]).slice(0,11)
+    var otu_labels = Object.values(inputdata.samples[candidate_index]["otu_labels"]).slice(0,11)
     otu_ids_str = otu_ids.map(convertAsString);
-    otu_ids_str.forEach(element => {
-        console.log(element)
-    });
-    // var sample_values = Object.values(SamplesArray[2]).slice(0,11)
-    // otu_ids.forEach(([key,value]) => {
-    //     console.log(key);
-    //     console.log(value);
-    // });
+
+    // meta data 
+    var candidate_meta_ethnicity = inputdata.metadata[candidate_index]["ethnicity"];
+    var candidate_meta_gender = inputdata.metadata[candidate_index]["gender"];
+    var candidate_meta_age = inputdata.metadata[candidate_index]["age"];
+    var candidate_meta_location = inputdata.metadata[candidate_index]["location"]
+    var candidate_meta_bbtype = inputdata.metadata[candidate_index]["bbtype"]
+    var candidate_meta_wfreq = inputdata.metadata[candidate_index]["wfreq"];
+    
+    
 
     console.log(typeof(otu_ids))
-    // var sample_values = Object.values(inputdata.samples[0].sample_values).forEach(value => console.log(value));
-    // var otu_ids = Object.values(inputdata.samples[0].otu_ids).forEach(value => console.log(value));
-    // numArray3.sort((firstNum, secondNum) => firstNum - secondNum);
-    BuildGraph(otu_ids_str, sample_values);
+
+    BuildBarGraph(otu_ids_str, sample_values, otu_labels);
+    BuildBubbleChart(otu_ids, sample_values, otu_labels);
+    BuildMetaTab(candidate_meta_ethnicity, candidate_meta_gender, candidate_meta_age, candidate_meta_location, candidate_meta_bbtype, candidate_meta_wfreq)
 };
 
 
-function BuildGraph(x_data,y_data) {
+function BuildBarGraph(x_data, y_data, hover_data) {
     var trace1 = {
-        x: x_data.map(element => element),
-        y: y_data,
-        type: "bar"
+        y: x_data.map(element => element),
+        x: y_data,
+        type: "bar",
+        text: hover_data,
+        orientation: "h"
     };
     console.log("BuildGraph")
     // x_data.forEach(element => {
@@ -97,7 +115,13 @@ function BuildGraph(x_data,y_data) {
     var data = [trace1];
 
     var chartLayout = {
-        title: "foo"
+        title: "Bacteria Colonies in Bellybutton (yum yum)",
+        // xaxis: {
+        //     autorange: "reversed"
+        // },
+        yaxis: {
+            autorange: "reversed"
+        }
     };
     Plotly.newPlot("bar", data, chartLayout);
 
@@ -109,18 +133,13 @@ function BuildGraph(x_data,y_data) {
 
 
 
-mainFunction()
 
-// function BuildArrays(inputData) {
-//     console.log("start BuildArray");
-//     Object.keys(inputData).forEach(key => console.log(key));
-// }
+mainFunction();
 
-// var JSONdata = ReadInJSONData(jsonfile);
+// d3.select("#selDataset")
+//     .on("change", optionChanged());
 
-// console.log(JSONdata);
-// console.log(typeof(JSONdata));
-
-// var myvar = Object.keys(JSONdata).forEach(key => console.log(key));
-// console.log("my var " + myvar);
-// console.log(JSON.stringify(JSONdata));
+function optionChanged() {
+    console.log("function optionChange")
+    ReadInJSONData(jsonfile)
+}
